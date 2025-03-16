afe5cc375fa2a7a72a52d5b0b80963423d142443
Ben Doggett <bdoggett@gmail.com>
2025-03-15 21:18:05 -0400

    Complete login server route and test.

--- app/server.js
+++ app/server.js
@@ -1,31 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
+const cors = require("cors");
+const uid = require("rand-token").uid;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+const users = require("../initial-data/users.json");
+const brands = require("../initial-data/brands.json");
+const products = require("../initial-data/products.json");
 
 // Error handling
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Starting the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+
+//Login
+app.post("/api/login", (request, response) => {
+  if (!request.body.username || !request.body.password) {
+    return response
+      .status(400)
+      .json({ message: "Incomplete login information provided" });
+  }
+  console.log("Request:", request.body);
+  const user = users.find((user) => {
+    return (
+      user.login.username == request.body.username &&
+      user.login.password == request.body.password
+    );
+  });
+  console.log("User:", user);
+
+  if (!user) {
+    return response.status(401).json({ message: "Invalid login credentials" });
+  }
+
+  const accessToken = { token: uid(16) };
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(accessToken));
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -3,13 +3,49 @@ const chaiHttp = require("chai-http");
 const server = require("../app/server"); // Adjust the path as needed
 // const randToken = require('rand-token')
 
-const should = chai.should();
+chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 
-describe("Brands", () => {});
+describe("Login", () => {
+  describe("POST /api/login", function () {
+    it("Should return an access token for authentication if login credentials are valid", function (done) {
+      const login = { username: "yellowleopard753", password: "jonjon" };
 
-describe("Login", () => {});
+      //arrange
+      chai
+        .request(server)
+        .post("/api/login")
+        .send(login)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("object");
+          res.body.should.have.property("token");
+          res.body.token.should.be.a("string");
+          done();
+        });
+    });
 
-describe("Cart", () => {});
+    it("Should return an error message for invalid login credentials", function (done) {
+      const login = { username: "wrongUsername", password: "wrongPassword" };
+
+      chai
+        .request(server)
+        .post("/api/login")
+        .send(login)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.an("object");
+          res.body.should.have
+            .property("message")
+            .eql("Invalid login credentials");
+          done();
+        });
+    });
+  });
+});
+
+// describe("Brands", () => {});
+
+// describe("Cart", () => {});
